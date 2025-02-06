4b1cb6c0770a925cdada0f9eade5ac5c1546d9d0
hp <hp@DESKTOP-4UOSIF4>
2025-02-06 08:59:43 +0800

    添加项目文件。

---
+++ Project1/1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout.setf(ios_base::fixed, ios_base::floatfield);
+	char ch1 = 65;
+	char ch2 = (64);
+	int i = ch2;
+	//cin >> ch1;
+	cout.put(ch1) << endl;
+	cout << ch1 << endl;
+	cout << ch2;
+	return 0;
+}
